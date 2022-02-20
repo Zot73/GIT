31f1770fb70e854b702e71cbaafc9fffd7eceadc
Denis Zotin <zotim73@list.ru>
2022-02-20 18:52:52 +0300

    задания по JS

---
+++ Easy/js/index.js
@@ -0,0 +1,11 @@
+const WEIGHT = 90;
+console.log("my weight is: " + WEIGHT   + ' kg');
+
+const C = 300000000;
+console.log("speed of light is: " + C  + ' m/s');
+
+
+let m = 90;
+let c = 90000000000000000;
+const energy = m*c;
+console.log("my energy is: " + energy);
